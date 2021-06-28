6f4be5398f29efced4a845062e73a16d9a0c7cf0
Roberto Boscolo <beto.boscolo@gmail.com>
2021-06-27 23:02:59 -0300

    Auth context saved in session!

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
 import { BrowserRouter, Route } from 'react-router-dom';
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 import firebase from "firebase";
 import { auth } from "./services/firebase";
 
@@ -19,6 +19,22 @@ export const AuthContext = createContext({} as AuthContextType);
 
 function App() {
   const [user, setUser] = useState<User>();
+
+  useEffect(() => {
+    auth.onAuthStateChanged(user => {
+      if (user) {
+        const { displayName, photoURL, uid } = user
+        if (!displayName || !photoURL) {
+          throw new Error('Missing information from Google Account.');
+        }
+        setUser({
+          id: uid,
+          name: displayName,
+          avatar: photoURL
+        })
+      }
+    })
+  }, [])
 
   async function signInWithGoogle() {
     const provider = new firebase.auth.GoogleAuthProvider();
