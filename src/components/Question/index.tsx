14342fa1aa4512b10a72bc706c108cc0b4146407
Roberto Boscolo <beto.boscolo@gmail.com>
2021-07-28 21:42:48 -0300

    New Like button component.

--- src/components/Question/index.tsx
+++ src/components/Question/index.tsx
@@ -1,17 +1,20 @@
+import { ReactNode } from 'react';
 import './styles.scss';
 
-type Questionprops = {
+type QuestionProps = {
     content: string;
     author: {
         name: string;
         avatar: string;
-    }
+    };
+    children?: ReactNode
 }
 
 export function Question({
     content, 
-    author
-}: Questionprops) {
+    author,
+    children
+}: QuestionProps) {
 
     return (
         <div className="question">
@@ -21,7 +24,9 @@ export function Question({
                     <img src={author.avatar} alt={author.name} />
                     <span>{author.name}</span>
                 </div>
-                <div></div>
+                <div>
+                    {children}
+                </div>
             </footer>
         </div>
     )
