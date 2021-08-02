2616d2b7c8e63fce66393623d48188cc2ade8605
Roberto Boscolo <beto.boscolo@gmail.com>
2021-08-01 23:08:52 -0300

    Like function adjusts.

--- src/hooks/useRoom.ts
+++ src/hooks/useRoom.ts
@@ -12,7 +12,7 @@ type QuestionType = {
     isAnswered: boolean;
     isHighlighted: boolean;
     likeCount: number;
-    hasLiked: boolean;
+    likeId: string | undefined ;
 }
 
 type FirebaseQuestions = Record<string, {
@@ -48,11 +48,11 @@ export function useRoom(roomId: string) {
                     isHighlighted: value.isHighlighted,
                     isAnswered: value.isAnswered,
                     likeCount: Object.values(value.likes ?? {}).length,
-                    hasLiked: Object.values(value.likes ?? {}).some(like => like.authorId === user?.id)
+                    likeId: Object.entries(value.likes ?? {}).find(([key, like])=> like.authorId === user?.id)?.[0]
                 }
             })
             setTitle(databaseRoom.title);
-            setQuestions(parsedQuestions)
+            setQuestions(parsedQuestions);
         })
         return () => {
             roomRef.off('value');
